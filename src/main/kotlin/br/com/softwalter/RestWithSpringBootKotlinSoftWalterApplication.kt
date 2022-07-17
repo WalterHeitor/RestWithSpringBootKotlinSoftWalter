db236601e88387ed22366e77877f032a26f7a3ca
walterheitor <walterheitorfo@gmail.com>
2022-07-17 15:48:40 -0300

    setup pprojeto

---
+++ src/main/kotlin/br/com/softwalter/RestWithSpringBootKotlinSoftWalterApplication.kt
@@ -0,0 +1,11 @@
+package br.com.softwalter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootKotlinSoftWalterApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootKotlinSoftWalterApplication>(*args)
+}
